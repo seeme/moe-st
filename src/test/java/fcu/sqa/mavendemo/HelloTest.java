385c4664ecb0b544eaea38a9901e1892260d9017
sammy <seeme.goo@gmail.com>
2016-07-11 22:51:29 +0800

    新增測試案例

--- src/test/java/fcu/sqa/mavendemo/HelloTest.java
+++ src/test/java/fcu/sqa/mavendemo/HelloTest.java
@@ -1,5 +1,17 @@
 package fcu.sqa.mavendemo;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
 public class HelloTest {
+	
+	@Test
+	public void testSayHello() {
+		Hello hello = new Hello();
+		String expected = "Hello Sammy!";
+		String actual = hello.sayHello("Sammy");
+		Assert.assertEquals(expected, actual);
+	}
 
 }
