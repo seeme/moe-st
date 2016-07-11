4e8f395870b0e0cf2c1dca42f561f11437d7b99d
sammy <seeme.goo@gmail.com>
2016-07-12 01:27:45 +0800

    修復Test Case

--- src/test/java/fcu/sqa/mavendemo/HelloTest.java
+++ src/test/java/fcu/sqa/mavendemo/HelloTest.java
@@ -9,7 +9,7 @@ public class HelloTest {
 	@Test
 	public void testSayHello() {
 		Hello hello = new Hello();
-		String expected = "Hello Sammy!";
+		String expected = "Hi Sammy!";
 		String actual = hello.sayHello("Sammy");
 		Assert.assertEquals(expected, actual);
 	}
